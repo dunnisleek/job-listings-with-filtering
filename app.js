723b55348af08b9894fa8d8d48aa8b29c179aeae
dunnisleek <94145981+dunnisleek@users.noreply.github.com>
2022-12-15 20:59:11 +0100

    initial.commit
    i update the search to filter case insensitive and adjusted the style

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-const app=Vue.createApp({
+const app = Vue.createApp({
   data() {
     return {
-      jobListing:[],
-      search:'',
-      
+      jobListing: [],
+      search: ""
     };
   },
   async mounted() {
     const res = await fetch("./data.json");
     const data = await res.json();
     this.jobListing = data;
+   
   },
-  computed:{
-       filterJobs(){
-            return this.jobListing.filter((job) =>{
-        
-              return job.role.match(this.search) || job.level.match(this.search) || job.company.match(this.search)
-              || job.location.match(this.search)
-           
-            })
-          }
-        },
-      methods:{
-          clear(){
-                this.search=''
-                  }
-   }
-  
+  computed: {
+    filterJobs() {
+      return this.jobListing.filter((job) => {
+        return job.role.toLowerCase().match(this.search.toLowerCase()) ||
+          job.level.toLowerCase().match(this.search.toLowerCase()) ||
+          job.company.toLowerCase().match(this.search.toLowerCase()) ||
+          job.location.toLowerCase().match(this.search.toLowerCase()) 
+      
+      });
+    },
+  },
+  methods: {
+    clear() {
+      this.search = "";
+    }
+  },
+//   watch: {
+//     'search' :function (job) {
+//         this.search = job.toLowerCase()
+//     } 
+// }
 });
-app.mount("#app");
+app.mount('#app');
